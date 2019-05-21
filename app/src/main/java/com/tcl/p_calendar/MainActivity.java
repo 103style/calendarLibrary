d10b692faf9b720164935c7ef7a8461e1098a157
xiaoke.luo <xiaoke.luo@tcl.com>
2019-05-21 16:40:29 +0800

    update

--- app/src/main/java/com/tcl/p_calendar/MainActivity.java
+++ app/src/main/java/com/tcl/p_calendar/MainActivity.java
@@ -7,9 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
-import org.threeten.bp.LocalDate;
-
-import java.util.Calendar;
 import java.util.HashSet;
 
 
@@ -24,22 +21,31 @@ public class MainActivity extends AppCompatActivity {
 
         calendarView = findViewById(R.id.test);
 
+        //设置月份格式
         calendarView.setTitleFormatter(new MonthDefaultTitleFormatter(getResources().getStringArray(R.array.month_array)));
 
-        calendarView.setSelectedDate(CalendarDay.today());
 
+        //设置月份的左右点击按钮
         calendarView.setLeftArrow(R.drawable.calendar_select_left);
         calendarView.setRightArrow(R.drawable.calendar_select_right);
 
+        // 设置右上角文字的颜色和文字
         calendarView.setTodayTextColor(R.color.calendar_taday_text_color);
         calendarView.setTvRightTopText(R.string.tv_today);
 
-//        calendarView.setSelectionDrawable(ContextCompat.getDrawable(this, R.drawable.calendar_select_right));
+        //选中今天
+        calendarView.setSelectedDate(CalendarDay.today());
 
+
+        //设置 今天之后的日期不能点击  并且不能滑动到 今天之后的月份
+        calendarView.setAfterTodayClickable(false);
+
+        // 设置 今天之前的10天 添加标记的日期
         HashSet<CalendarDay> dates = new HashSet<>();
+        CalendarDay today = CalendarDay.today();
         for (int i = 0; i < 10; i++) {
-            CalendarDay c = CalendarDay.from(LocalDate.of(2019, 3, 10 + i));
-            dates.add(c);
+            today = today.toPreviousDay();
+            dates.add(today);
         }
         calendarView.addDecorator(new EventDecorator(Color.parseColor("#FF01E9C3"),
                 getResources().getDisplayMetrics().widthPixels / 120, dates));
